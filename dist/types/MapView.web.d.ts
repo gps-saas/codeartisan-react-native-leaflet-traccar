cb8d1997be96bed2a06b8fd858b7ee29608f6811
Rafael Pedroso <codeartisan@Dons-MacBook-Air.local>
2024-12-02 23:01:57 -0300

    Adjust react-native version

--- dist/types/MapView.web.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import { MapViewProps } from './MyTypes';
-export default function MapView({ debug, markerCenter, markers, region, zoom, fitBound, showMarkerClicked, mapOnClick, mapOnMove, mapOnMoveEnd }: MapViewProps): React.JSX.Element;
--- dist/types/MyHTML.d.ts
+++
@@ -1,2 +0,0 @@
-import { MyHTMLProps } from "./MyTypes";
-export declare const MyHTML: ({ debug, region, markers, markerCenter, zoom, fitBound, showMarkerClicked, showAttribution, mapOptions, }: MyHTMLProps) => string;
